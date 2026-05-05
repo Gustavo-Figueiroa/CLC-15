29f6c155ed16df6efa808a62fdaa43279aa4fc55
Gustavo-Figueiroa <gustavos.figueiroa@hotmail.com>
2026-05-05 15:34:03 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,25 @@ resource "aws_vpc" "terraform_vpc" {
 
   tags = {
     Name = "tf-vpc"
+    CC = "123456"
+    Owner = "DevOps"
+  }
+}
+
+# Correcao primeira issue
+resource "aws_flow_log" "example" {
+  log_destination      = "arn:aws:s3:::clc15-vini-terraform"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.terraform_vpc.id
+}
+
+# Correcao segunda issue
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.terraform_vpc.id
+  
+  tags = {
+    Name = "my-iac-sg"
   }
 }
 
